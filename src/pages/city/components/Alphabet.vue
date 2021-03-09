a5238d70dbdec5e80bd2923339139d07cf1a81bb
Greatpride <2201923799@qq.com>
2021-03-09 23:58:04 +0800

    myError

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,37 @@
+<template>
+    <ul class="list">
+        <li class="item-word">a</li>
+        <li class="item-word">a</li>
+        <li class="item-word">a</li>
+        <li class="item-word">a</li>
+        <li class="item-word">a</li>
+        <li class="item-word">a</li>
+        <li class="item-word">a</li>
+        <li class="item-word">a</li>
+        <li class="item-word">a</li>
+        <li class="item-word">a</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name:'alphabet'
+}
+</script>
+
+<style lang="stylus" rel='stylesheet/stylus' scoped>
+  @import '~styles/varibles.styl'
+  .list
+    display flex 
+    flex-direction column
+    justify-content center
+    position absolute
+    top 1.58rem
+    right 0
+    bottom 0
+    width .4rem
+    .item-word
+      line-height .4rem
+      text-align  center
+      color $bgColor
+</style>

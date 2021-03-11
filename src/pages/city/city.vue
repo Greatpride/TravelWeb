6afbf67b5b8de16c43f7df9f5aab2694121c3990
Greatpride <2201923799@qq.com>
2021-03-12 00:03:41 +0800

    cityajax

--- src/pages/city/city.vue
+++ src/pages/city/city.vue
@@ -1,21 +1,50 @@
 <template>
   <div>
     <city-header></city-header>
-    <city-search></city-search>
+    <city-search :cities="cities"></city-search>
+    <city-list :cities="cities" :hot="hotCities" ></city-list>
+    <city-alphabet :cities="cities"></city-alphabet>
   </div>
 </template>
 
 <script>
-import CityHeader from './components/header'
-import CitySearch from './components/search'
+import axios from "axios";
+import CityHeader from "./components/header";
+import CitySearch from "./components/search";
+import CityList from "./components/list";
+import CityAlphabet from "./components/Alphabet";
 export default {
-    name: 'city',
-    components:{
-        CityHeader,
-        CitySearch
+  name: "City",
+  components: {
+    CityHeader,
+    CitySearch,
+    CityList,
+    CityAlphabet
+  },
+  data() {
+    return {
+      cities: {},
+      hotCities: []
+    };
+  },
+  methods: {
+    getCityInfo() {
+      axios.get("/api/city.json").then(this.handleGetCityInfoSucc);
+    },
+    handleGetCityInfoSucc(res) {
+      res = res.data;
+      if (res.ret && res.data) {
+        const data = res.data;
+        this.cities = data.cities;
+        this.hotCities = data.hotCities;
+      }
     }
-}
+  },
+  mounted() {
+    this.getCityInfo();
+  }
+};
 </script>
 
-<style lang='stylus' scoped>
+<style lang="stylus" scoped>
 </style>
---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,31 @@
+<template>
+    <ul class="list">
+        <li class="item-word" v-for="(item,key) of cities" :key="key">{{key}}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name:'alphabet',
+    props:{
+      cities:Object
+    }
+}
+</script>
+
+<style lang="stylus" rel='stylesheet/stylus' scoped>
+  @import '~styles/varibles.styl'
+  .list
+    display flex 
+    flex-direction column
+    justify-content center
+    position absolute
+    top 1.58rem
+    right 0
+    bottom 0
+    width .4rem
+    .item-word
+      line-height .4rem
+      text-align  center
+      color $bgColor
+</style>

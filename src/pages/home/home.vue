5dafb765cd73872b98aa1d9d84388422d8173e95
Greatpride <2201923799@qq.com>
2021-03-17 09:14:34 +0800

    use vuex

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <home-header :city="city"></home-header>
+        <home-header></home-header>
         <home-swiper :list = "swiperList"></home-swiper>
         <home-icons :icons="iconList"></home-icons>
         <home-recommend :list = "recommendList"></home-recommend>
@@ -27,7 +27,6 @@ export default {
     },
     data () {
       return {
-          city: '',
           swiperList: [],
           iconList:[],
           recommendList:[],
@@ -44,7 +43,6 @@ export default {
             if (res.ret&&res.data){
                 const data = res.data
                 console.log(data)
-                this.city = data.city
                 this.swiperList = data.bannerList
                 this.iconList = data.iconList
                 this.recommendList = data.recommendList
